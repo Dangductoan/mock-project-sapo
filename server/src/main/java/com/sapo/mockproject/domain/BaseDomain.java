47019d412af01817498adc2e3065b067525a7393
KhutxIT <khutx.it@gmail.com>
2022-05-25 14:04:04 +0700

    refactor: remove create_at and modified_at

--- server/src/main/java/com/sapo/mockproject/domain/BaseDomain.java
+++ server/src/main/java/com/sapo/mockproject/domain/BaseDomain.java
@@ -1,11 +1,8 @@
 package com.sapo.mockproject.domain;
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.time.Instant;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
@@ -15,14 +12,6 @@ public class BaseDomain<ID extends Number> {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private ID id;
 
-    @Column(name = "created_at", updatable = false)
-    @CreatedDate
-    private Instant createdAt;
-
-    @Column(name = "modified_at")
-    @LastModifiedDate
-    private Instant modifiedAt;
-
     public ID getId() {
         return id;
     }
@@ -31,19 +20,4 @@ public class BaseDomain<ID extends Number> {
         this.id = id;
     }
 
-    public Instant getModifiedAt() {
-        return modifiedAt;
-    }
-
-    public void setModifiedAt(Instant modifiedAt) {
-        this.modifiedAt = modifiedAt;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }
 }
