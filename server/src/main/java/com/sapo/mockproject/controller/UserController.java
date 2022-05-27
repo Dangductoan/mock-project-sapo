1a2bacf6881486bb1cd2f4b52cd6e9e6485260c8
noon111101 <trieu11112001@gmail.com>
2022-05-27 11:58:37 +0700

    fix : user service

--- server/src/main/java/com/sapo/mockproject/controller/UserController.java
+++ server/src/main/java/com/sapo/mockproject/controller/UserController.java
@@ -20,7 +20,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/chief-accountant/")
+@RequestMapping("/api/v1/chief/")
 public class UserController extends BaseController<Integer, UserDTO> {
 
     public UserController(GenericService<Integer, UserDTO> genericService) {
