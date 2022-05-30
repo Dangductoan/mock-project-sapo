9c94934e28d7fd6804b2914c7ccd2f329ecfec59
noon111101 <trieu11112001@gmail.com>
2022-05-30 14:30:09 +0700

    feat : authencontext react

---
+++ client/src/api/AccountantService.js
@@ -0,0 +1,36 @@
+import call_api from "./Request";
+
+
+
+const getAccountant = () => {
+  return call_api({
+    method: "GET",
+    // url: "chief-accountant/user"
+    url:"chief/"
+  })
+}
+
+const createAccountant = (accountant) => {
+  return call_api({
+    method: "POST",
+    url: "chief-accountant/user/",
+    data: accountant
+  })
+}
+
+const updateAccountant = (id, accountant) => {
+  return call_api({
+    method: "PUT",
+    url: `chief-accountant/user/${id}`,
+    data: accountant
+  })
+}
+
+
+const AccountantService = {
+   getAccountant,
+  createAccountant,
+  updateAccountant,
+}
+
+export default AccountantService;
--- client/src/component/sidebar/AccountantSidebar.jsx
+++ client/src/component/sidebar/AccountantSidebar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./SideBar.css";
+import "./sidebar.css";
 import { Link } from "react-router-dom";
 function SidebarAcc() {
   return (
