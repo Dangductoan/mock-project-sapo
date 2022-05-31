36f3c57121259b70e36ab6f5c8c01678498eb04a
KhutxIT <khutx.it@gmail.com>
2022-05-31 16:14:47 +0700

    feat: bill-client-crud

--- client/src/page/bill/billlist/BillListPage.jsx
+++ client/src/page/bill/billlist/BillListPage.jsx
@@ -1,5 +1,132 @@
+import { useHistory, useRouteMatch, Link } from "react-router-dom";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
+import SearchIcon from "@mui/icons-material/Search";
+import MaterialPagination from "../../../component/pagination/template/MaterialPagination";
+import "./BillListPage.css";
+import { useEffect, useState } from "react";
+import BillService from "../../../api/BillService";
+
+const itemPerPage = 10;
+
 function BillListPage() {
-  return <div>Bill List Page</div>;
+  let user = JSON.parse(localStorage.getItem("user"));
+  const match = useRouteMatch();
+  const history = useHistory();
+
+  const [bills, setBills] = useState([]);
+  const [totalItem, setTotalItem] = useState(0);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+
+  let totalPage =
+    totalItem % itemPerPage === 0
+      ? totalItem / itemPerPage
+      : Math.floor(totalItem / itemPerPage) + 1;
+
+  useEffect(() => {
+    BillService.searchBill({ query: query, page: 0, size: itemPerPage })
+      .then((res) => setBills(res.data?.bills))
+      .catch((err) => console.log(err));
+  }, [query]);
+
+  useEffect(() => {
+    BillService.count()
+      .then((res) => setTotalItem(res.data))
+      .catch((err) => console.log(err));
+  }, [totalItem]);
+
+  const handlePaginationChange = (page) => {
+    setPage(page);
+
+    BillService.searchBill({ query: query, page: page - 1, size: itemPerPage })
+      .then((res) => setBills(res.data?.bills))
+      .catch((err) => console.log(err));
+  };
+
+  const searchBill = (e) => {
+    e.preventDefault();
+
+    BillService.searchBill({ query: query, page: 0, size: itemPerPage })
+      .then((res) => setBills(res.data?.bills))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="bill-list">
+      <div className="bill-header">
+        <h2>Phiếu thu</h2>
+        <div className="bill-header__right">
+          <p>Xin chào {user.name} </p>
+          <Link to={match.path}>Logout</Link>
+        </div>
+      </div>
+      <div className="bill-option">
+        <div className="option-excel">
+          <FileDownloadIcon />
+          <span>Xuất file</span>
+          <FileUploadIcon />
+          <span>Nhập file</span>
+        </div>
+        <button
+          className="btn-create"
+          onClick={() => history.push(`${match.path}/create`)}
+        >
+          Tạo phiếu thu
+        </button>
+      </div>
+      <div className="bill-list-content">
+        <div className="bill-list-filter">
+          <div className="bill-searchbar">
+            <SearchIcon />
+            <form onSubmit={searchBill}>
+              <input
+                type="text"
+                placeholder="Tìm theo..."
+                onKeyPress={(e) => {
+                  if (e.key === "Enter") setQuery(e.target.value);
+                }}
+              />
+            </form>
+          </div>
+        </div>
+        <div className="content__table">
+          <table>
+            <thead>
+              <tr>
+                <th>Mã phiếu</th>
+                <th>Loại phiếu</th>
+                <th>Hình thức thanh toán</th>
+                <th>Trạng thái</th>
+                <th>Người tạo</th>
+                <th>Số tiền thu</th>
+                <th>Ngày ghi nhận</th>
+              </tr>
+            </thead>
+            <tbody>
+              {bills.map((bill) => (
+                <tr key={bill.id}>
+                  <td>{bill.code}</td>
+                  <td>{bill.billCategory.name}</td>
+                  <td>{bill.payment}</td>
+                  <td>{bill.created_by}</td>
+                  <td>{bill.totalValue}</td>
+                  <td>{bill.created_at}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="pagination">
+          <MaterialPagination
+            count={totalPage}
+            page={page}
+            onChange={handlePaginationChange}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BillListPage;
--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -1,77 +1,82 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { DefaultLayout } from "../layout/DefaultLayout";
+import AccountantLayout from "../layout/accountantlayout/AccountantLayout";
+import { ChiefLayout } from "../layout/ChiefLayout";
+
 import Home from "../page/home/Home";
 import About from "../page/about/About";
 import Login from "../page/login/Login";
 import Support from "../page/support/Support";
 import Customer from "../page/customer/Customer";
-import { DefaultLayout } from "../layout/DefaultLayout";
-import { AccountantLayout } from "../layout/AccountantLayout";
-import { ChiefLayout } from "../layout/ChiefLayout";
 import NotFound from "../page/notfound/NotFound";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ChiefAccountant from "../page/chiefAccountant/ChiefAccountant";
 import BillCategory from "../page/billCategory/BillCategory";
 import Accountant from "../page/accountant/Accountant";
 import ManageAccountant from "../page/manageaccountant/ManageAccountant";
 import ManageCustomer from "../page/managecustomer/ManageCustomer";
-import {AuthProvider} from '../component/authencontext/AuthenProvide'
+import BillListPage from "../page/bill/billlist/BillListPage";
+import BillAdd from "../page/bill/billadd/BillAdd";
+
+import { AuthProvider } from "../component/authencontext/AuthenProvide";
 import { RequireAuth } from "../component/authencontext/RequireAuth";
 
 function RouterDefined() {
   return (
-   <AuthProvider>
-    <Router>
-   < RequireAuth>
-      <Switch>
-        <Route path="/accountant/:path?" exact>
-          <AccountantLayout>
-            <Switch>
-              <Route path="/accountant" exact component={Accountant} />
-              <Route path="/*" exact component={NotFound} />
-            </Switch>
-          </AccountantLayout>
-        </Route>
-        <Route path="/chief-accountant/:path?" exact>
-          <ChiefLayout>
-            <Switch>
-              <Route
-                path="/chief-accountant"
-                exact
-                component={ChiefAccountant}
-              />
-              <Route
-                path="/chief-accountant/bill-category"
-                component={BillCategory}
-              />
-               <Route
-                path="/chief-accountant/user"
-                component={ManageAccountant}
-              />
-               <Route
-                path="/chief-accountant/customer"
-                component={ManageCustomer}
-              />
-              <Route path="/*" exact component={NotFound} />
-            </Switch>
-          </ChiefLayout>
-        </Route>
+      <Router>
+        <RequireAuth>
+          <Switch>
+            <Route path="/accountant/:path?/:path?" exact>
+              <AccountantLayout>
+                <Switch>
+                  <Route path="/accountant" exact component={Accountant} />
+                  <Route path="/accountant/bills" exact component={BillListPage} />
+                  <Route path="/accountant/bills/create" exact component={BillAdd} />
+                  <Route path="/*" exact component={NotFound} />
+                </Switch>
+              </AccountantLayout>
+            </Route>
+            <Route path="/chief-accountant/:path?" exact>
+              <ChiefLayout>
+                <Switch>
+                  <Route
+                    path="/chief-accountant"
+                    exact
+                    component={ChiefAccountant}
+                  />
+                  <Route
+                    path="/chief-accountant/bill-category"
+                    component={BillCategory}
+                  />
+                  <Route
+                    path="/chief-accountant/user"
+                    component={ManageAccountant}
+                  />
+                  <Route
+                    path="/chief-accountant/customer"
+                    component={ManageCustomer}
+                  />
+                  <Route path="/*" exact component={NotFound} />
+                </Switch>
+              </ChiefLayout>
+            </Route>
 
-      <Route>
-          <DefaultLayout>
-            <Switch>
-              <Route path="/about" component={About} />
-              <Route path="/customer" component={Customer} />
-              <Route path="/login" component={Login} />
-              <Route path="/support" component={Support} />
-              <Route path="/" exact component={Home} />
-              <Route path="/*" exact component={NotFound} />
-            </Switch>
-          </DefaultLayout>
-        </Route>
-      </Switch>
-      </RequireAuth>
-    </Router>
-    </AuthProvider>
+            <Route>
+              <DefaultLayout>
+                <Switch>
+                  <Route path="/about" component={About} />
+                  <Route path="/customer" component={Customer} />
+                  {/* <Route path="/login" component={Login} /> */}
+                  <Route path="/support" component={Support} />
+                  <Route path="/" exact component={Home} />
+                  <Route path="/*" exact component={NotFound} />
+                </Switch>
+              </DefaultLayout>
+            </Route>
+          </Switch>
+        </RequireAuth>
+      </Router>
   );
 }
 
