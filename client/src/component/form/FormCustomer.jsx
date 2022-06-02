2169542539bf32c7f4dc844ed99683a61b2222c3
noon111101 <trieu11112001@gmail.com>
2022-06-02 12:04:34 +0700

    feat: Customer crud

---
+++ client/src/component/form/FormCustomer.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react'
+import './Form.css'
+import CustomerService from '../../api/CustomerService'
+function FormCustomer({ title, action, show, setShow, id , cus,handleCreate,handleUpdate }) {
+    const [type,setType]=useState(action === "Thêm"?true:false)
+    const [customer, setCustomer] = useState(action === 'Thêm'?{
+        code:"",
+        name:"",
+        phone:"",
+        groupCustomer:"",
+        createdBy:"",
+        address:""
+       
+  
+    }:
+    {
+        code:cus.code,
+        name:cus.name,
+        phone:cus.phoneNumber,
+        groupCustomer:cus.groupCustomer,
+        createdBy:cus.createdBy,
+        address:cus.address
+   
+  
+    }
+    )
+    const customerForUpdate = {id:id,...customer}
+    const handleChange = (e) => {
+        setCustomer({ ...customer, [e.target.name]: e.target.value })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+      
+        if(action === 'Thêm') {
+            CustomerService.createCustomer(customer).then(res=>handleCreate())
+
+        } else {
+            CustomerService.updateCustomer(id, customerForUpdate).then(res=>handleUpdate())
+
+        }          
+      
+        setShow(!show)
+    }
+    const handleClick = () => {
+        setShow(!show)
+    }
+    return (
+        <>
+            <div className="modal">
+                <form onSubmit={handleSubmit} className="form">
+                    <div className="form-title">
+                        <h3>{title}</h3>
+                        <span onClick={handleClick}>X</span>
+                    </div>
+                    <div className="form-content">
+                       {type && <> <label htmlFor="">Mã khách hàng</label><br></br>
+                        <input onChange={handleChange} name="code" value={customer.code} type="text"  />   </>}
+
+                        <label htmlFor="">Tên khách hàng*</label><br></br>
+                        <input onChange={handleChange} name="name" value={customer.name} type="text" />
+
+                        <label htmlFor="">Số điện thoại</label><br></br>
+                        <input onChange={handleChange} name="phoneNumber" value={customer.phoneNumber} type="text" />
+
+                        <label htmlFor="">Nhóm khách hàng</label><br></br>
+                        <input onChange={handleChange} name="groupCustomer" value={customer.groupCustomer} type="text" />
+
+                        <label htmlFor="">Nhân viên phụ trách</label><br></br>
+                        <input onChange={handleChange} name="createdBy" value={customer.createdBy} type="text" />
+
+                        <label htmlFor="">Địa chỉ</label><br></br>
+                        <input onChange={handleChange} name="address" value={customer.address} type="text" />
+                    </div>
+                    <div className="form-btn">
+                        <button className='btn btn-no-active' onClick={handleClick}>Thoát</button>
+                        <button className='btn' >{action}</button>
+                    </div>
+                </form>
+
+            </div>
+
+            <div className="overlay"></div>
+
+
+        </>
+    )
+}
+
+export default FormCustomer
---
+++ client/src/component/list/ListCustomer.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import './ListCustomer.css'
+function ListCustomer(props) {
+    const handleClick = () => {
+      props.setCustomer({
+        code:props.code,
+        name:props.name,
+        phone:props.phoneNumber,
+        groupCustomer:props.groupCustomer,
+        createdBy:props.createdBy,
+        address:props.address
+      })
+      props.setShow(!props.show)
+      props. setIndex(props.index)
+    }
+  
+    const handleChange = (id) => {
+  
+    }
+    return (
+      <div className="list" >
+          <div><input class="checkCustomer" type="checkbox"  onChange={()=>handleChange(props.index)} data-indeterminate="false" value=""/></div>
+          <h3 className='list-item' onClick={handleClick}>{props.code}</h3>
+          <h3 className='list-item' onClick={handleClick}>{props.name}</h3>
+          <h3 className='list-item' onClick={handleClick}>{props.phoneNumber}</h3>
+          <h3 className='list-item' onClick={handleClick}>{props.groupCustomer}</h3>
+          <h3 className='list-item' onClick={handleClick}>{props.createdBy}</h3>
+          <h3 className='list-item' onClick={handleClick}>{props.address}</h3>
+          <h3 className='list-item' onClick={handleClick}>{props.createdAt}</h3>
+
+
+      </div>
+    )
+}
+
+export default ListCustomer;
