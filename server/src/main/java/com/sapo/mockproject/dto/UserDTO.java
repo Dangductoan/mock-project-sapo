6b70a76cb40c28deb8bb6d521aa9391600761a2d
noon111101 <trieu11112001@gmail.com>
2022-06-06 22:58:36 +0700

    feat : customer

--- server/src/main/java/com/sapo/mockproject/dto/UserDTO.java
+++ server/src/main/java/com/sapo/mockproject/dto/UserDTO.java
@@ -14,16 +14,19 @@ import javax.validation.constraints.NotNull;
 @Setter
 public class UserDTO extends BaseDTO<Integer> {
 
-    @NotNull
+    @NotNull(message = "Chưa nhập tên đăng nhập")
     private String username;
 
-    @NotNull
+    @NotNull(message = "Chưa nhập tên mật khẩu")
     private String password;
 
+    @NotNull(message = "Chưa nhập tên ")
     private String name;
 
+    @NotNull(message = "Chưa nhập số điện thoại")
     private String phoneNumber;
 
+    @NotNull(message = "Chưa nhập địa chỉ")
     private String address;
 
     private String token;
