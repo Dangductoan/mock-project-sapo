b4571d0fea360be7c5e6e83082953754e5b242cb
KhutxIT <khutx.it@gmail.com>
2022-06-14 09:50:09 +0700

    refactor: update search condition

--- server/src/main/java/com/sapo/mockproject/repository/BillRepository.java
+++ server/src/main/java/com/sapo/mockproject/repository/BillRepository.java
@@ -1,33 +1,31 @@
 package com.sapo.mockproject.repository;
 
 import com.sapo.mockproject.domain.Bill;
-import com.sapo.mockproject.domain.RevenueStats;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BillRepository extends GenericRepository<Bill, Long> {
 
+    String SEARCH_CONDITION = " FROM Bill b INNER JOIN b.billCategory bc INNER JOIN b.customer c " +
+            " WHERE b.payment LIKE %?1% OR b.code LIKE %?1% OR b.description LIKE %?1% OR b.createdBy LIKE %?1%" +
+            " OR bc.name LIKE %?1% OR c.name LIKE %?1%";
+
     @Override
-    @Query("SELECT b FROM Bill b INNER JOIN b.billCategory bc WHERE b.payment LIKE %?1% OR b.code LIKE %?1%" +
-            " OR b.description LIKE %?1% OR b.createdBy LIKE %?1% OR b.modifiedBy LIKE %?1% OR bc.name LIKE %?1%")
+    @Query("SELECT b " + SEARCH_CONDITION)
     Page<Bill> fetchByQuery(String query, Pageable pageable);
 
     @Override
-    @Query("SELECT b FROM Bill b INNER JOIN b.billCategory bc WHERE b.payment LIKE %?1% OR b.code LIKE %?1%" +
-            " OR b.description LIKE %?1% OR b.createdBy LIKE %?1% OR b.modifiedBy LIKE %?1% OR bc.name LIKE %?1%")
+    @Query("SELECT b " + SEARCH_CONDITION)
     List<Bill> fetchByQuery(String query);
 
     @Override
-    @Query("SELECT COUNT(b) FROM Bill b INNER JOIN b.billCategory bc WHERE b.payment LIKE %?1% OR b.code LIKE %?1%" +
-            " OR b.description LIKE %?1% OR b.createdBy LIKE %?1% OR b.modifiedBy LIKE %?1% OR bc.name LIKE %?1%")
+    @Query("SELECT COUNT(b) " + SEARCH_CONDITION)
     Long countSearch(String query);
 
     Optional<Bill> findByCode(String code);
