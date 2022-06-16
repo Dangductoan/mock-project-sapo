17e269b47f4cbee51c157ac46bbefdeb08f20dac
KhutxIT <khutx.it@gmail.com>
2022-06-16 22:08:44 +0700

    feat: bill filter

--- server/src/main/java/com/sapo/mockproject/service/impl/BillServiceImpl.java
+++ server/src/main/java/com/sapo/mockproject/service/impl/BillServiceImpl.java
@@ -9,6 +9,7 @@ import com.sapo.mockproject.repository.BillRepository;
 import com.sapo.mockproject.repository.GenericRepository;
 import com.sapo.mockproject.repository.RevenueStatsRepository;
 import com.sapo.mockproject.repository.UserRepository;
+import com.sapo.mockproject.repository.custom.CustomBillRepository;
 import com.sapo.mockproject.security.UserDetailsImpl;
 import com.sapo.mockproject.service.BillService;
 import com.sapo.mockproject.service.mapper.GenericMapper;
@@ -21,6 +22,7 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -28,14 +30,18 @@ public class BillServiceImpl extends BaseServiceImpl<Long, BillDTO, Bill> implem
 
     private final BillRepository billRepository;
 
+    private final CustomBillRepository customBillRepository;
+
     private final RevenueStatsRepository revenueStatsRepository;
 
     private final UserRepository userRepository;
 
     public BillServiceImpl(GenericRepository<Bill, Long> genericRepository, GenericMapper<Long, BillDTO, Bill> genericMapper,
-                           RevenueStatsRepository revenueStatsRepository, UserRepository userRepository) {
+                           CustomBillRepository customBillRepository, RevenueStatsRepository revenueStatsRepository,
+                           UserRepository userRepository) {
         super(genericRepository, genericMapper);
         this.billRepository = (BillRepository) genericRepository;
+        this.customBillRepository = customBillRepository;
         this.revenueStatsRepository = revenueStatsRepository;
         this.userRepository = userRepository;
     }
@@ -94,6 +100,7 @@ public class BillServiceImpl extends BaseServiceImpl<Long, BillDTO, Bill> implem
         Optional<RevenueStats> revenueStats = revenueStatsRepository.findByStringDate(formatter.format(billDTO.getCreatedAt()));
         if (revenueStats.isPresent()) {
             revenueStats.get().setTotalRevenue(revenueStats.get().getTotalRevenue() - bill.getTotalValue() + billDTO.getTotalValue());
+            revenueStatsRepository.save(revenueStats.get());
         }
         // end modify Revenue Stats
 
@@ -105,5 +112,20 @@ public class BillServiceImpl extends BaseServiceImpl<Long, BillDTO, Bill> implem
     @Override
     public List<BillDTO> fetchBetweenDate(String start, String end) {
         return genericMapper.toDto(billRepository.fetchBetweenDate(start, end));
+    }
+
+    @Override
+    public List<BillDTO> filter(Map<String, String> requestParams, Integer page, Integer size) {
+        return genericMapper.toDto(customBillRepository.filter(requestParams, page, size));
+    }
+
+    @Override
+    public List<BillDTO> filter(Map<String, String> requestParams) {
+        return genericMapper.toDto(customBillRepository.filter(requestParams));
+    }
+
+    @Override
+    public Long countFilter(Map<String, String> requestParams) {
+        return customBillRepository.countFilter(requestParams);
     }
 }
