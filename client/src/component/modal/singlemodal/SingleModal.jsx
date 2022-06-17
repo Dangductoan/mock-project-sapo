1aea312aa768bc639a8ed3eca686db16e995c872
KhutxIT <khutx.it@gmail.com>
2022-06-17 15:16:20 +0700

    feat: upgrade single modal

--- client/src/component/modal/singlemodal/SingleModal.jsx
+++ client/src/component/modal/singlemodal/SingleModal.jsx
@@ -5,18 +5,30 @@ import CloseIcon from "@mui/icons-material/Close";
 
 import "./SingleModal.css";
 
-export default function SingleModal({ open, setOpen, title, onConfirm }) {
+export default function SingleModal({
+  open,
+  setOpen,
+  title,
+  onConfirm,
+  children,
+  ...operators
+}) {
   const closeModal = () => setOpen(false);
+
+  let className = operators.hasOwnProperty("className")
+    ? operators.className
+    : "";
 
   return (
     <>
       {open && (
         <Modal>
-          <div className="single-modal">
+          <div className={`single-modal ${className}`}>
             <div className="single-modal-title">
               <h3>{title}</h3>
               <CloseIcon onClick={closeModal} />
             </div>
+            {children && <div className="single-modal-content">{children}</div>}
             <div className="single-modal-footer">
               <button className="btn-cancle" onClick={closeModal}>
                 Hủy
